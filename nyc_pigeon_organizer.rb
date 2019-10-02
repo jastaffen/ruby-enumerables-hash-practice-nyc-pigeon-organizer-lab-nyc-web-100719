d13d32ab2c77d9339d4680e8d7e8fa78f5d760d2
Jack Staffen <jack.staffen@gmail.com>
2019-10-02 18:25:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   data.each do |key, value|
-    array = value.values.flatten
+    array = key.keys.flatten
     array.each do |name|
       new_hash[name] = {:color => [], :gender =>[], :lives =>[] }
       binding.pry
