83f6109f478afcc8503fb1fc2bd973aeb2c3f2d8
Jack Staffen <jack.staffen@gmail.com>
2019-10-02 21:30:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |subkey, names|
+      names.each do |name|
+
+        if !new_hash[name]
+
+          new_hash[name] = {}
+
+
+        end
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << subkey.to_s
+
+      #  if !new_hash[name][key] = [subkey.to_s]
+      #    new_hash[name][key] = [].push([subkey.to_s])
+      #  end
+
+      end
+    end
+  end
+
+  new_hash
 end
