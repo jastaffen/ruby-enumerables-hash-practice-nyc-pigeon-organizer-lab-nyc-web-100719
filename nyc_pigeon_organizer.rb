5013d8a0469fba77df44e038a46e948fd6f09360
Jack Staffen <jack.staffen@gmail.com>
2019-10-02 18:10:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,6 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
     array = value.values.flatten
-    key.keys
     array.each do |name|
       new_hash[el] = 1
       binding.pry
