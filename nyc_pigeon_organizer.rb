2510c201bd9b9f4bfebe654732c1f7f7ec99645d
Jack Staffen <jack.staffen@gmail.com>
2019-10-02 20:58:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,7 +21,7 @@ def nyc_pigeon_organizer(data)
        if !new_hash[name][key]
          new_hash[name][key] = []
        end
-       if !new_hash[name][key] = [subkey.to_s] && key[subkey].include?(name)
+       if !new_hash[name][key] = [subkey.to_s] && value[subkey].include?(name)
          new_hash[name][key] = [].push([subkey.to_s])
        end
      end
